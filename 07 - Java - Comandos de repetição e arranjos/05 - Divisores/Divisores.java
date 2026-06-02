e84f139c764f93f1cd28c66036789e232a54c96f
Lucas <lucasgodoyjor@gmail.com>
2026-06-02 20:20:35 -0300

    Adiciona rascunho do exercício divisores

--- 07 - Java - Comandos de repetição e arranjos/05 - Divisores/Divisores.java
+++ 07 - Java - Comandos de repetição e arranjos/05 - Divisores/Divisores.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Divisores {    
 
-     public static void main(String[] args){
+    public static void main(String[] args){
+        System.out.print("Entre com um inteiro positivo: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         
+        System.out.print("Divisores [");
+        for (int i=1; i<=n; i++){
+                
+            if (n % i == 0){
+                System.out.print(i);
+                System.out.print(", ");                
+            }
+        
+        }
+            System.out.printf("]%n");
+        sc.close();
     }
 
 }
