2fc9adfbcd47bf0b885ddd2aeb187c91e8c361df
Lucas <lucasgodoyjor@gmail.com>
2026-06-02 20:12:40 -0300

    Adiciona exercício faixas de ph com for

--- 07 - Java - Comandos de repetição e arranjos/04 - Peixes/Peixes.java
+++ 07 - Java - Comandos de repetição e arranjos/04 - Peixes/Peixes.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
 public class Peixes {
     
     public static void main(String[] args){
         
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entre com o pH mínimo: ");
+        double phMinimo = sc.nextDouble();
 
+        System.out.print("Entre com o pH máximo: ");
+        double phMaximo = sc.nextDouble();
+
+        System.out.print("Entre com o número de medições: ");
+        int numMedicoes = sc.nextInt();
+
+        int foraDaFaixa = 0;
+
+        for (int i=1; i<=numMedicoes; i++){
+            System.out.print("Entre com a medição " + i + ": ");
+            double phMedicao = sc.nextDouble();
+
+            if (phMedicao <= phMinimo || phMedicao >= phMaximo){
+                foraDaFaixa++;
+            }
+        }
+        System.out.println("Número de medições fora da faixa: " + foraDaFaixa);
+    }
 
 }
