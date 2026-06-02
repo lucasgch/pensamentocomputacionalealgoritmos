e3822456da0bb4210938a88fed3e7c50a014c906
Lucas <Lucasg.06@aluno.ifsc.edu.br>
2026-06-02 16:59:29 -0300

    Adiciona exercício de cálculo de nível mínimo para obter vitalidade com switch e while

--- 10 - Extra - Parte 2/12 - Jogo intepretacao papeis/Rpg.java
+++ 10 - Extra - Parte 2/12 - Jogo intepretacao papeis/Rpg.java
@@ -4,8 +4,52 @@ public class Rpg {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int classe;
+        
         
         System.out.print("Entre com a classe (1-arqueiro, 2-bárbaro, 3-druida, 4-mago): ");
-    }    
+
+        int classe = scanner.nextInt();
+
+        
+        
+        switch (classe) {
+            case 1 -> {
+                System.out.print("Entre com a vitalidade desejada: ");
+                int vitalidadeDesejada = scanner.nextInt();
+                int nivelMinimo = nivelMinimo(150, 10, vitalidadeDesejada);
+                System.out.println("Nível necessário: "+nivelMinimo);
+            }
+            case 2 ->{
+                System.out.print("Entre com a vitalidade desejada: ");
+                int vitalidadeDesejada = scanner.nextInt();
+                int nivelMinimo = nivelMinimo(200, 15, vitalidadeDesejada);
+                System.out.println("Nível necessário: "+nivelMinimo);
+            }
+            case 3, 4 ->{
+                System.out.print("Entre com a vitalidade desejada: ");
+                int vitalidadeDesejada = scanner.nextInt();
+                int nivelMinimo = nivelMinimo(100, 5, vitalidadeDesejada);
+                System.out.println("Nível necessário: "+nivelMinimo);
+            }
+            default -> System.out.println("Classe inválida!");
+        }
+                
+    }
+    
+    static int nivelMinimo(int v0, int inc, int v){
+        int nivelMinimo = 0;
+        
+        while (v0<v){
+
+            nivelMinimo++;
+            
+            if (nivelMinimo%5==0){
+                v0+=inc*2;
+            } else {
+                v0+=inc;
+            }
+            
+        }
+        return nivelMinimo;
+    }
 }
