ecf323a23bfac17a145589a44a9aadc4f2bc68ad
Lucas <lucasgodoyjor@gmail.com>
2026-06-02 19:22:13 -0300

    Adiciona exercício de ocorrencias numéricas com do-while

--- 10 - Extra - Parte 2/14 - Ocorrencias Maior Valor/MaiorValor.java
+++ 10 - Extra - Parte 2/14 - Ocorrencias Maior Valor/MaiorValor.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class MaiorValor {
     
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre com inteiros positivos. Entre com 0 ou um inteiro negativo para terminar.");
+        int entrada = 0;
+        int maior = 0;
+        int ocorrenciasIguaisOuMaior = 0;
+
+
+        do{
+            entrada = scanner.nextInt();
+            if (entrada > maior){
+                maior = entrada;
+                ocorrenciasIguaisOuMaior = 1;
+            }
+            else if (entrada == maior){
+                ocorrenciasIguaisOuMaior++;
+            }
+
+        } while (entrada > 0);
+
+        System.out.println("O maior inteiro foi " + maior  + " e ele ocorreu " + ocorrenciasIguaisOuMaior + " vezes.");
         
+        scanner.close();
     }
 
 }
