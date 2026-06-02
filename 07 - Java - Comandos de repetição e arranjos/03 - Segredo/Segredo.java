0ffaf1436ad8b7f830a0bff8957f7da5b1a9a368
Lucas <lucasgodoyjor@gmail.com>
2026-06-02 19:40:20 -0300

    Adiciona exercício segredo com do-while

---
+++ 07 - Java - Comandos de repetição e arranjos/03 - Segredo/Segredo.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Segredo {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        int segredo = new Random().nextInt(101);
+        System.out.println("Bem-vindo ao jogo do segredo!");
+        int palpite = 0;
+        int tentativas = 0;
+        do{
+            tentativas++;
+            System.out.print("Entre com seu palpite (1 a 100): ");
+            palpite = scanner.nextInt();
+            if (palpite < segredo) {
+                System.out.println("O segredo é maior");
+            } else if (palpite > segredo) {
+                System.out.println("O segredo é menor");
+            }
+        } while ( palpite != segredo );
+
+        scanner.close();
+        System.out.println("Parabéns, você acertou em " + tentativas + " tentativas!");
+    }
+
+}
