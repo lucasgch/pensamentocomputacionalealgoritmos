c9c8ace9cdbdf13451a3a02cfc12791f0bbcd4c9
Lucas <Lucasgodoyjor@gmail.com>
2026-06-08 14:40:15 -0300

    Adiciona ex. 20 - Notas turma com loop for

--- 10 - Extra - Parte 2/20 - Notas da turma/NotasTurma.java
+++ 10 - Extra - Parte 2/20 - Notas da turma/NotasTurma.java
@@ -9,11 +9,22 @@ public class NotasTurma {
         int provas = scanner.nextInt();
 
         double mediaTurma = 0;
-        
+
         for (int i=1; i<=alunos; i++){
             int somaNotas = 0;
+            int qtNotas = 1;
             System.out.printf("Entre com as notas do aluno %d: ", i);
-            somaNotas+=scanner.nextDouble();
+            while (qtNotas<=provas){
+                somaNotas+=scanner.nextDouble();                
+                qtNotas++;
+            }
+            double media = somaNotas/provas;
+            if (media>=6){
+                System.out.printf("Aluno %d aprovado com média %.2f %n", i, media);
+            } else {
+                System.out.printf("Aluno %d reprovado com média %.2f %n", i, media);
+            }
         }
+        scanner.close();
     }
 }
